dbed2fbe1afee99c915de633eadd6cc073f3e055
Dubjay18 <jejeniyi7@gmail.com>
2024-05-31 08:40:46 +0100

    fix: order create method

--- common/errors.go
+++ common/errors.go
@@ -1,1 +1,7 @@
 package common
+
+import "errors"
+
+var (
+	ErrorNoItems = errors.New("items must be at least one")
+)
--- gateway/handler.go
+++ gateway/handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	common "github.com/Dubjay18/OMS-common.git"
 	pb "github.com/Dubjay18/OMS-common.git/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net/http"
 )
 
@@ -26,8 +29,42 @@ func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 		common.WriteError(w, http.StatusBadRequest, err)
 		return
 	}
-	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+	if err := validateItems(items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
 		CustomerID: CustomerID,
 		Items:      items,
 	})
+
+	rStatus := status.Convert(err)
+
+	if rStatus != nil {
+		if rStatus.Code() == codes.InvalidArgument {
+			common.WriteError(w, http.StatusBadRequest, rStatus.Err())
+			return
+		}
+	}
+	if err != nil {
+		common.WriteError(w, http.StatusInternalServerError, err)
+		return
+
+	}
+	common.WriteJson(w, http.StatusCreated, o)
+}
+
+func validateItems(items []*pb.ItemsWithQuantity) error {
+	if len(items) == 0 {
+		return common.ErrorNoItems
+	}
+	for _, item := range items {
+		if item.ItemID == "" {
+			return errors.New("item_id is required")
+		}
+		if item.Quantity <= 0 {
+			return errors.New("quantity must be greater than 0")
+		}
+	}
+	return nil
 }
